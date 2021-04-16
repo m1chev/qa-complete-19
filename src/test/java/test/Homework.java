3ce402d85f575cd1aecd48be607fd46d027080c7
Aleksandar Karamfilov <aleksandar.karamfilov.extern@joyn.de>
2021-04-16 11:01:41 +0300

    Added homework

--- src/test/java/test/Homework.java
+++ src/test/java/test/Homework.java
@@ -2,9 +2,20 @@ package test;
 
 public class Homework {
 
-    //
+   /*
+    Your homework is to create the below tests using UI/API interactions
+    If necessary add more methods to ClientPage
+     */
 
-    //
 
-    //
+    //Create test for search client by name
+
+
+    //Create test for search client by mol
+
+
+    // Create test for search client by address
+
+
+    //Create test for search which does not match existing client
 }
